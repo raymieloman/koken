2436cbb8d3e74705e921396beeca360fb030d848
Raymond Loman <raymond@carpago.nl>
2024-09-24 16:17:13 +0200

    Add kitchenbuilder

--- src/main/java/nl/acme/koken/builder/Kitchen.java
+++ src/main/java/nl/acme/koken/builder/Kitchen.java
@@ -13,6 +13,12 @@ public class Kitchen {
     private String kookstel;
     private Color color;
 
+    private Kitchen(KitchenBuilder builder) {
+        this.gasfornuis = builder.gasfornuis;
+        this.kookstel = builder.kookstel;
+        this.color = builder.color;
+    }
+
     public static class KitchenBuilder {
         private String gasfornuis;
         private String kookstel;
@@ -22,6 +28,18 @@ public class Kitchen {
             this.gasfornuis = gasfornuis; // de verplichte velden van het te maken domeinobject moeten hier in de constructor.
         }
 
+        public KitchenBuilder kookstel(String kookstel) {
+            this.kookstel = kookstel;
+            return this;
+        }
 
+        public KitchenBuilder color(Color color) {
+            this.color = color;
+            return this;
+        }
+
+        public Kitchen build() {
+            return new Kitchen(this);
+        }
     }
 }
