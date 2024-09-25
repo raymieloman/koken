48656704564eeda6c032b51e93aeaa22da752e83
Raymond Loman <raymond@carpago.nl>
2024-09-25 09:46:08 +0200

    Add democode for IO

---
+++ src/main/java/nl/acme/koken/io/exercise1/App.java
@@ -0,0 +1,31 @@
+package nl.acme.koken.io.exercise1;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+    public static final String fileName = "myfood.txt";
+    public static final List<String> foodList = Arrays.asList("Potatoes", "Hamburger", "Milk");
+
+    public static void main(String[] args) throws IOException {
+        createFile(fileName);
+        readFile(fileName);
+    }
+
+    public static void createFile(String fileName) throws IOException {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (var food : foodList) {
+                writer.write(food);
+                writer.newLine();
+            }
+        }
+    }
+    
+    public static void readFile(String fileName) throws IOException {
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            reader.lines().forEach(System.out::println);
+        }
+    }
+}
