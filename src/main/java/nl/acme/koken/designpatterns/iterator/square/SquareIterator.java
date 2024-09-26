7f5baeda6b929693135d10b78549994be498e2aa
Raymond Loman <raymond@carpago.nl>
2024-09-26 11:29:22 +0200

    min max

--- src/main/java/nl/acme/koken/designpatterns/iterator/square/SquareIterator.java
+++ src/main/java/nl/acme/koken/designpatterns/iterator/square/SquareIterator.java
@@ -3,17 +3,13 @@ package nl.acme.koken.designpatterns.iterator.square;
 import java.util.Iterator;
 
 public class SquareIterator implements Iterator<Integer> {
-
-    private int min;
     private int max;
     private int current;
 
     public SquareIterator(int min, int max) {
-        this.min = min;
         this.max = max;
         this.current = min;
     }
-
 
     @Override
     public boolean hasNext() {
