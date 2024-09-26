5ca35aea9e0fd0097688154212979a3eb9b60b4d
Raymond Loman <raymond@carpago.nl>
2024-09-26 15:27:42 +0200

    Add more for concurrency and regex

---
+++ src/main/java/nl/acme/koken/concurrency/bonus/App.java
@@ -0,0 +1,27 @@
+package nl.acme.koken.concurrency.bonus;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        List<Integer> numbers = new CopyOnWriteArrayList<>();
+        numbers.addAll(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89));
+
+        System.out.println(numbers);
+
+        for (int element : numbers) {
+            if (element % 2 == 0 ) {
+                if (element < numbers.size()) {
+                    numbers.remove(Integer.valueOf(element));
+                }
+
+            }
+        }
+        System.out.println(numbers);
+    }
+}
+
+
