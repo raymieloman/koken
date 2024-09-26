79c24695e20aca146795e24696e686d2abb1149d
Raymond Loman <raymond@carpago.nl>
2024-09-26 16:17:26 +0200

    Add NIO-2 visitor

---
+++ src/main/java/nl/acme/koken/nio2/visitor/NotSourceRelatedFilesVisitor.java
@@ -0,0 +1,31 @@
+package nl.acme.koken.nio2.visitor;
+
+import lombok.Data;
+import lombok.ToString;
+
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@ToString
+public class NotSourceRelatedFilesVisitor extends SimpleFileVisitor<Path> {
+    public static final String REGEX = "\\.git|target|\\.idea|\\.java";
+
+    private int fileCounter;
+    private int size;
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        Pattern pattern = Pattern.compile(REGEX);
+        Matcher matcher = pattern.matcher(file.toFile().getAbsolutePath());
+        if (!matcher.find()) { // find is a occurrence in the String, matches is an exact match. Hence it's name! :-)
+            fileCounter++;
+            size += file.toFile().length();
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+}
